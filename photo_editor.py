85685f43d5ce9da04e1157472a6d246058685e7a
Dimitri Dias Moreira <taikamya@outlook.com>
2022-08-02 10:33:27 +0200

    Updating and adding

--- photo_editor.py
+++ photo_editor.py
@@ -10,8 +10,8 @@ edited_folder = "./editedPhotos/"
 def sharpen(file: str) -> None:
     '''This function applies the SHARPEN filter to an image file. '''
     # test photo
-    test_photo = f"{photos_folder}" + file
-    img = Image.open(f"{test_photo}")
+    test_photo = photos_folder + file
+    img = Image.open(test_photo)
     # Slight sharpen effect
     filtered_img = img.filter(ImageFilter.SHARPEN)
     # Clean filename to save it as something else
@@ -23,13 +23,14 @@ def sharpen(file: str) -> None:
 def sharpen_all() -> None:
     '''This function applies the SHARPEN filter to all photos inside a folder. '''
     for filename in os.listdir(photos_folder):
-        img = Image.open(f"{photos_folder}/{filename}")
+        img = Image.open(photos_folder + filename)
         # Changed WxH resolution of the image and tilted to 90º. Used "rotate" to correct that.
-        filtered_img = img.filter(ImageFilter.SHARPEN).rotate(+90)
+        filtered_img = img.filter(ImageFilter.SHARPEN)
         # Clean filename to save it as something else
         clean_filename = os.path.splitext(filename)[0]
-        filtered_img.save(f"{edited_folder}/{clean_filename}_edited.jpg")
+        filtered_img.save(f"{edited_folder}{clean_filename}_edited.jpg")
 
 
 if __name__=="__main__":
-    sharpen("test.jpg")
+    # sharpen("face.jpg")
+    sharpen_all()
